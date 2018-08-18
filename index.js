395739783c281c8f634b4334673bf9f29466be6d
Aleksandra Niewiera <218427@student.pwr.edu.pl>
2018-08-18 19:33:46 +0200

    Working initial memory version

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let clickingAllowed = true;
 let matchingList = [];
 let movesCounter = 0;
-
+const game = document.getElementById("game");
 createTiles=()=>{
     removeAllCards();
     let duplicatedNumbers = duplicateCards(numbers);
@@ -11,12 +11,13 @@ createTiles=()=>{
     var card = document.createElement("div");
     card.className= "cont";
     card.appendChild(
-    document.createTextNode(shuffled[i])
+    document.createTextNode("Click me!")
     );
     card.dataset.id = i;
+    card.dataset.clicked = "false";
     card.dataset.key = shuffled[i];
     card.addEventListener("click", cardClicked.bind(this, card));
-    document.getElementById("game").appendChild(card);
+    game.appendChild(card);
     }
 }
 duplicateCards=(cards)=>{
@@ -28,12 +29,15 @@ return duplicatedNumbers.sort(() => Math.random() - 0.5);
 }
  
 cardClicked = card=>{
-document.getElementById("score").innerHTML = ++movesCounter;
 console.log(card.dataset.key);
+card.dataset.clicked = "true";
+hideOthersContent();
+card.innerHTML=card.dataset.key;
 if(matchingList.length < 2 && matchingList[0]!=card){
     matchingList.push(card);
 }
 if(matchingList.length == 2){
+    document.getElementById("score").innerHTML = ++movesCounter;
     if(matchingList[0].dataset.key == matchingList[1].dataset.key)
         {
         console.log("Matches!");
@@ -43,11 +47,23 @@ if(matchingList.length == 2){
 
     matchingList = [];
 }
-}
+};
 
 removeAllCards=()=>{
+     matchingList = [];
+     movesCounter = 0;
+    document.getElementById("score").innerHTML ="0";
     let game = document.getElementById("game");
     while (game.firstChild) {
       game.removeChild(game.firstChild);
     }
+};
+
+hideOthersContent = ()=>{
+for (var i of game.children) {
+  if (i.dataset.clicked === "true"){
+    i.innerHTML = "Click Me!";
 }
+}
+
+};
