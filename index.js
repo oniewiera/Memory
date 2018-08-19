fd6c6dae514bc8514dd335e7229dd9108ee5d339
Aleksandra Niewiera <218427@student.pwr.edu.pl>
2018-08-19 15:00:25 +0200

    Little changes

--- index.js
+++ index.js
@@ -7,52 +7,52 @@ const cards = [
     id: 2,
     src: "img/two.png"
   },
-     {
-        id: 3,
-        src: "img/three.png",
-    },
-       {
-        id: 4,
-        src: "img/four.png"
-    },
-       {
-        id: 5,
-        src: "img/five.png"
-    },
-       {
-        id: 6,
-        src: "img/six.png"
-    },
-       {
-        id: 7,
-        src: "img/seven.png"
-    },
-       {
-        id: 8,
-        src: "img/eight.png"
-    },
-       {
-        id: 9,
-        src: "img/nine.png"
-    },
-    {
-        id: 10,
-        src: "img/ten.png"
-    }
+  {
+    id: 3,
+    src: "img/three.png"
+  },
+  {
+    id: 4,
+    src: "img/four.png"
+  },
+  {
+    id: 5,
+    src: "img/five.png"
+  },
+  {
+    id: 6,
+    src: "img/six.png"
+  },
+  {
+    id: 7,
+    src: "img/seven.png"
+  },
+  {
+    id: 8,
+    src: "img/eight.png"
+  },
+  {
+    id: 9,
+    src: "img/nine.png"
+  },
+  {
+    id: 10,
+    src: "img/ten.png"
+  }
 ];
 
 let matchingList = [];
 let movesCounter = 0;
 const game = document.getElementById("game");
 
-createTiles=()=>{
-    showGrid();
-    removeAllCards();
-    let duplicatedNumbers = duplicateCards(cards);
-    let shuffled = shuffleTable(duplicatedNumbers);
-    for (let i = 0; i<shuffled.length;i++){
+createTiles = () => {
+  showGrid();
+  removeAllCards();
+  let duplicatedNumbers = duplicateCards(cards);
+  let shuffled = shuffleTable(duplicatedNumbers);
+  for (let i = 0; i < shuffled.length; i++) {
     let card = document.createElement("div");
-    card.className= "cont";
+    card.className = "cont";
     card.dataset.id = i;
     card.id = `card${i}`;
     card.dataset.clicked = "false";
@@ -63,78 +63,74 @@ createTiles=()=>{
     $(`#${card.id}`).flip({ trigger: "manual" });
 
     card.addEventListener("click", cardClicked.bind(this, card));
-    }
-}
-
-duplicateCards=(cards)=>{
-    return cards.concat(cards);
-}
-
-prepareBackAndFront = (card)=>{
-    let front = document.createElement("div");
-    front.className = "front";
-    let back = document.createElement("div");
-    back.className = "back";
-    back.style.background = `url(${card.dataset.bg})`;
-    card.appendChild(front);
-    card.appendChild(back);
+  }
 };
 
-shuffleTable = duplicatedNumbers=>{
-return duplicatedNumbers.sort(() => Math.random() - 0.5);
-}
+duplicateCards = cards => {
+  return cards.concat(cards);
+};
 
-showGrid=()=>{
-     game.style.visibility = "visible";
-}
- 
-cardClicked = card=>{
-$(`#${card.id}`).flip(true);
-if (matchingList.length == 0)
-    hideOthersContent(matchingList);
+prepareBackAndFront = card => {
+  let front = document.createElement("div");
+  front.className = "front";
+  let back = document.createElement("div");
+  back.className = "back";
+  back.style.background = `url(${card.dataset.bg})`;
+  card.appendChild(front);
+  card.appendChild(back);
+};
 
+shuffleTable = duplicatedNumbers => {
+  return duplicatedNumbers.sort(() => Math.random() - 0.5);
+};
 
-card.dataset.clicked = "true";
-if(matchingList.length < 2 && matchingList[0]!=card)
+showGrid = () => {
+  game.style.visibility = "visible";
+};
+
+cardClicked = card => {
+  $(`#${card.id}`).flip(true);
+  if (matchingList.length == 0) hideOthersContent(matchingList);
+
+  card.dataset.clicked = "true";
+  if (matchingList.length < 2 && matchingList[0] != card)
     matchingList.push(card);
 
-if(matchingList.length == 2){
-    checkIfGameOver() ? (document.getElementById("par").innerHTML = "Congratulations!") : document.getElementById("score").innerHTML = ++movesCounter;
-    if(matchingList[0].dataset.key == matchingList[1].dataset.key){
-        matchingList[0].style.visibility = "hidden";
-        matchingList[1].style.visibility = "hidden";
+  if (matchingList.length == 2) {
+    if (matchingList[0].dataset.key == matchingList[1].dataset.key) {
+      matchingList[0].style.visibility = "hidden";
+      matchingList[1].style.visibility = "hidden";
     }
-
     matchingList = [];
-}
-};
-
-removeAllCards=()=>{
-    document.getElementById("par").innerHTML = "Train your brain.";
-    document.getElementById("start").innerText = "RESTART";
-     matchingList = [];
-     movesCounter = 0;
-    document.getElementById("score").innerHTML ="0";
-    let game = document.getElementById("game");
-    while (game.firstChild) {
-      game.removeChild(game.firstChild);
-    }
-};
-
-hideOthersContent = (matchingList)=>{
-for (var element of game.children) {
-  if (element.dataset.clicked == "true"){
-   $(`#${element.id}`).flip(false);
-    console.log(element.id);
-    element.dataset.clicked = 'false';
+    checkIfGameOver() ? (document.getElementById("moves").innerHTML = "Congratulations! Total moves:") : (document.getElementById("score").innerHTML = ++movesCounter);
   }
-}
 };
 
-checkIfGameOver=()=>{
-    for(var cards of game.children){
-        if(cards.style.visibility != "hidden")
-            return false;
+
+removeAllCards = () => {
+  document.getElementById("par").innerHTML = "Train your brain.";
+  document.getElementById("start").innerText = "RESTART";
+  document.getElementById("score").innerHTML = "0";
+  matchingList = [];
+  movesCounter = 0;
+  while (game.firstChild) {
+    game.removeChild(game.firstChild);
+  }
+};
+
+hideOthersContent = matchingList => {
+  for (var element of game.children) {
+    if (element.dataset.clicked == "true") {
+      $(`#${element.id}`).flip(false);
+      console.log(element.id);
+      element.dataset.clicked = "false";
     }
-    return true;
-}
+  }
+};
+
+checkIfGameOver = () => {
+  for (var cards of game.children) {
+    if (cards.style.visibility != "hidden") return false;
+  }
+  return true;
+};
